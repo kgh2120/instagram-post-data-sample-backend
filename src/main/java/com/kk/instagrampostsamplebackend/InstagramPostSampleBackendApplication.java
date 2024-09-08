e968ec890439a8491a4dc9a02d2ec8b61f1676f8
kyoohyun <kgh2120@gmail.com>
2024-09-09 02:29:30 +0900

    init pjt

---
+++ src/main/java/com/kk/instagrampostsamplebackend/InstagramPostSampleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kk.instagrampostsamplebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramPostSampleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramPostSampleBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kk/instagrampostsamplebackend/InstagramPostSampleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kk.instagrampostsamplebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramPostSampleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
